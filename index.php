238ecd28745fd7add50169615618979e6c5bcb25
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-08-23 00:51:02 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>Business Plan | The Polyfen Group</title>
+
+    <!-- INCLUDE HEAD.PHP start -->
+    <?php include 'includes/head.php';?>
+    <!-- INCLUDE HEAD.PHP end -->
+
+  </head>
+
+  <body>
+    <main>
+        <h1>Our Business Plan</h1>
+    </main>
+  </body>
+</html>
