92fa831b34558048e91c6c0d9f3e0be176b00504
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-08-26 23:16:15 +0200

    Update overview.php

--- content/overview.php
+++ content/overview.php
@@ -1,10 +1,9 @@
   <h2 id="overview" class="heading-1">1. Overview</h2>
   <p>We explore new ways of using technology to succeed in creative projects. The Polyfen Group LLC is a small holding company that owns and operates a portfolio of brands.</p>
-  <p>The company was founded in 2018 by Boris Hrncic &mdash;Argentinian-Croatian art director and product designer&mdash; with the support of a few colleagues sharing the same passion for building something we can be proud of.</p>
+  <p>The company was founded in 2018 by <a href="https://boris.hr" target="_blank">Boris Hrn&ccaron;i&cacute;</a> &mdash;Argentinian-Croatian art director and product designer&mdash; with the support of a few colleagues sharing the same passion for building something we can be proud of.</p>
   <p>The stock is 100% independently owned by the team with no dependency on investors or any other kind of external funding.</p>
   <p>Our location-independent business model takes advantage of the latest advancements in technology to work seamlessly with a fully-remote team years before the Covid pandemic made it the new normal.</p>
-    <p>Most of our attention focuses on our creative agency, Polyfen.</p>
-    <p>However, our full portfolio of brands include:</p>
+    <p>Most of our attention focuses on our creative agency, Polyfen. However, our full portfolio of brands include:</p>
     <ul class="hanging-list spaced-list arrow-bullet">
       <li><strong>Polyfen:</strong> Creative agency offering design, marketing, and software development services for new and emerging businesses. <a href="https://polyfen.com" target="_blank">polyfen.com</a></li>
       <li><strong>Poly Atlas:</strong> Open-source step-by-step guide to the Branding process, including practical tips, case studies, template files, and many other useful resources. <a target="_blank" href="http://polyatlas.wiki">polyatlas.wiki</a></li>
