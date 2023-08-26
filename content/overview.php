892e657eadedfdcfbd4ce276d0f7f6bb2ae605e4
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-08-26 23:29:19 +0200

    Update overview.php

--- content/overview.php
+++ content/overview.php
@@ -3,13 +3,34 @@
   <p>The company was founded in 2018 by <a href="https://boris.hr" target="_blank">Boris Hrn&ccaron;i&cacute;</a> &mdash;Argentinian-Croatian, art director and product designer&mdash; with the support of a few colleagues sharing the same passion for building something we can be proud of.</p>
   <p>The stock is 100% independently owned by the team with no dependency on investors or any other kind of external funding.</p>
   <p>Our location-independent business model takes advantage of the latest advancements in technology to work seamlessly with a fully-remote team years before the Covid pandemic made it the new normal.</p>
-    <p>Most of our attention focuses on our creative agency, Polyfen. However, our full portfolio of brands include:</p>
-    <ul class="hanging-list spaced-list arrow-bullet">
-      <li><strong>Polyfen:</strong> Creative agency offering design, marketing, and software development services for new and emerging businesses. <a href="https://polyfen.com" target="_blank">polyfen.com</a></li>
-      <li><strong>Poly Atlas:</strong> Open-source step-by-step guide to the Branding process, including practical tips, case studies, template files, and many other useful resources. <a target="_blank" href="http://polyatlas.wiki">polyatlas.wiki</a></li>
-      <li><strong>Poly Cookies</strong>: Open-source solution to privacy policy implementing a cookie consent banner with Google Tag Manager. <a href="http://polycookies.com" target="_blank">polycookies.com</a></li>
-      <li><strong>Poly Qwerty:</strong> <a href="https://polyqwerty.com" target="_blank">polyqwerty.com</a></li>
-      <li><strong>Poly Forum:</strong> Non-profit organization dedicated to promoting free and open education and professional growth in the creative industry. <a href="http://polyforum.org" target="_blank">polyforum.org</a> (Work in Progress)</li>
-      <li><strong>Poly Nucleus:</strong> <a href="https://polynucleus.com" target="_blank">polynucleus.com</a> (Work in Progress)</li>
-    </ul>
-    <p>And we have a few more projects in the oven...</p>
+  <p><strong>Most of our attention focuses on Polyfen,</strong> our creative agency. However, our full portfolio of brands include:</p>
+  <section id="sub-brands-container" style="padding-left:40px;padding-top:40px;">
+    <section class="sub-brand">
+      <img src="imgs/sub-brands/polyfen-logo.svg" alt="Polyfen">
+      <p>Creative agency offering design, marketing, and software development services for new and emerging businesses.</p>
+      <a href="https://polyfen.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyfen.com</a>
+    </section>
+    <hr>
+    <section class="sub-brand">
+      <img src="imgs/sub-brands/poly-atlas-logo.svg" alt="Poly Atlas">
+      <p>Open-source step-by-step guide to the Branding process, including practical tips, case studies, template files, and many other useful resources.</p>
+      <a href="https://polyatlas.wiki" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyatlas.wiki</a>
+    </section>
+    <hr>
+    <section class="sub-brand">
+      <img src="imgs/sub-brands/poly-forum-logo.svg" alt="Poly Forum">
+      <p>Non-profit organization dedicated to promoting free and open education and professional growth in the creative industry.</p>
+      <a href="https://polyforum.org" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyforum.org</a>
+    </section>
+    <hr>
+    <section class="sub-brand">
+      <img src="imgs/sub-brands/poly-cookies-logo.svg" alt="Poly Cookies">
+      <p>Open-source solution to privacy policy implementing a cookie consent banner with Google Tag Manager.</p>
+      <a href="https://polycookies.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polycookies.com</a>
+    </section>
+    <hr>
+    <section class="sub-brand" style="display:none;">
+      <img src="imgs/sub-brands/poly-qwerty-logo.svg" alt="Poly Qwerty">
+    </section>
+  </section>
+  <p>And we have a few more projects in the oven...</p>
