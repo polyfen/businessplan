c97daffbc5c4aab872b6fe4fda025b7667abc014
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-08-26 23:32:48 +0200

    Update overview.php

--- content/overview.php
+++ content/overview.php
@@ -6,30 +6,30 @@
   <p><strong>Most of our attention focuses on Polyfen,</strong> our creative agency. However, our full portfolio of brands include:</p>
   <section id="sub-brands-container" style="padding-left:40px;padding-top:40px;">
     <section class="sub-brand">
-      <img src="imgs/sub-brands/polyfen-logo.svg" alt="Polyfen">
+      <img src="imgs/sub-brands/polyfen-logo.svg" alt="Polyfen" height="60px" width="auto">
       <p>Creative agency offering design, marketing, and software development services for new and emerging businesses.</p>
       <a href="https://polyfen.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyfen.com</a>
     </section>
     <hr>
     <section class="sub-brand">
-      <img src="imgs/sub-brands/poly-atlas-logo.svg" alt="Poly Atlas">
+      <img src="imgs/sub-brands/poly-atlas-logo.svg" alt="Poly Atlas" height="60px" width="auto">
       <p>Open-source step-by-step guide to the Branding process, including practical tips, case studies, template files, and many other useful resources.</p>
       <a href="https://polyatlas.wiki" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyatlas.wiki</a>
     </section>
     <hr>
     <section class="sub-brand">
-      <img src="imgs/sub-brands/poly-forum-logo.svg" alt="Poly Forum">
-      <p>Non-profit organization dedicated to promoting free and open education and professional growth in the creative industry.</p>
-      <a href="https://polyforum.org" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyforum.org</a>
-    </section>
-    <hr>
-    <section class="sub-brand">
-      <img src="imgs/sub-brands/poly-cookies-logo.svg" alt="Poly Cookies">
+      <img src="imgs/sub-brands/poly-cookies-logo.svg" alt="Poly Cookies" height="60px" width="auto">
       <p>Open-source solution to privacy policy implementing a cookie consent banner with Google Tag Manager.</p>
       <a href="https://polycookies.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polycookies.com</a>
     </section>
     <hr>
-    <section class="sub-brand" style="display:none;">
+    <section class="sub-brand">
+      <img src="imgs/sub-brands/poly-forum-logo.svg" alt="Poly Forum" height="60px" width="auto">
+      <p>Non-profit organization dedicated to promoting free and open education and professional growth in the creative industry.</p>
+      <a href="https://polyforum.org" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyforum.org</a>
+    </section>
+    <hr>
+    <section class="sub-brand" style="display:none;" height="60px" width="auto">
       <img src="imgs/sub-brands/poly-qwerty-logo.svg" alt="Poly Qwerty">
     </section>
   </section>
