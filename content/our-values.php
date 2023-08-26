ade3349b402160847b86a38043eeae8bb5e9a6d3
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-08-26 21:34:45 +0200

    Update

--- content/our-values.php
+++ content/our-values.php
@@ -1,6 +1,6 @@
 <h2 id="core-values" class="heading-2">3. Our Values</h2>
 <p>At its core, The Polyfen Group is founded on —not business values, but— human values.</p>
-<ul class="hanging-list">
+<ul class="hanging-list arrow-bullet">
     <li><strong>Transparency</strong></li>
     <li><strong>Collaboration</strong></li>
     <li><strong>Education</strong></li>
