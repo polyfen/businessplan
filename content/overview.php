4802b70682b702ce4de883047d819f446b129613
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-08-26 23:12:18 +0200

    Update overview.php

--- content/overview.php
+++ content/overview.php
@@ -1,14 +1,16 @@
   <h2 id="overview" class="heading-1">1. Overview</h2>
-    <p><a href="https://thepolyfengroup.com" target="_blank">The Polyfen Group</a> is a small corporation parent of multiple brands related to software development and the creative industry.</p>
-    <p>We explore new ways of using technology to succeed in times of constant change.</p>
-    <p>The Polyfen Group (internally referred to as “TPG” or “Poly”) emerged from a network of creatives, engineers, marketing people, and others from the start-up scene. It was founded in 2018 by <a href="https://boris.hr"target="_blank">Boris Hrnčić</a>, Argentinian-Croatian art director and product designer.</p>
-    <p>Our location-independent business model takes advantage of the latest advancements in technology to work seamlessly with a fully-remote team years before the Covid pandemic made it the new normal.</p>
-    <p>Most of our work focuses on our creative and software development agency, Polyfen. However, our full portfolio of brands include:</p>
+  <p>We explore new ways of using technology to succeed in creative projects. The Polyfen Group LLC is a small holding company that owns and operates a portfolio of brands.</p>
+  <p>The company was founded in 2018 by Boris Hrncic &mdash;Argentinian-Croatian art director and product designer&mdash; with the support of a few colleagues sharing the same passion for building something we can be proud of.</p>
+  <p>The stock is 100% independently owned by the team with no dependency on investors or any other kind of external funding.</p>
+  <p>Our location-independent business model takes advantage of the latest advancements in technology to work seamlessly with a fully-remote team years before the Covid pandemic made it the new normal.</p>
+    <p>Most of our attention focuses on our creative agency, Polyfen.</p>
+    <p>However, our full portfolio of brands include:</p>
     <ul class="hanging-list spaced-list arrow-bullet">
-      <li><strong>Polyfen:</strong> Creative agency offering creative and software development services for new and emerging businesses. <a href="https://polyfen.com" target="_blank">polyfen.com</a></li>
+      <li><strong>Polyfen:</strong> Creative agency offering design, marketing, and software development services for new and emerging businesses. <a href="https://polyfen.com" target="_blank">polyfen.com</a></li>
       <li><strong>Poly Atlas:</strong> Open-source step-by-step guide to the Branding process, including practical tips, case studies, template files, and many other useful resources. <a target="_blank" href="http://polyatlas.wiki">polyatlas.wiki</a></li>
       <li><strong>Poly Cookies</strong>: Open-source solution to privacy policy implementing a cookie consent banner with Google Tag Manager. <a href="http://polycookies.com" target="_blank">polycookies.com</a></li>
       <li><strong>Poly Qwerty:</strong> <a href="https://polyqwerty.com" target="_blank">polyqwerty.com</a></li>
       <li><strong>Poly Forum:</strong> Non-profit organization dedicated to promoting free and open education and professional growth in the creative industry. <a href="http://polyforum.org" target="_blank">polyforum.org</a> (Work in Progress)</li>
       <li><strong>Poly Nucleus:</strong> <a href="https://polynucleus.com" target="_blank">polynucleus.com</a> (Work in Progress)</li>
     </ul>
+    <p>And we have a few more projects in the oven...</p>
