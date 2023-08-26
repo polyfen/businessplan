a290bb08499cdb42c1c26b2ff6146f6f3a5a33f6
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-08-26 21:42:46 +0200

    Update overview.php

--- content/overview.php
+++ content/overview.php
@@ -1,5 +1,5 @@
   <h2 id="overview" class="heading-2">1. Overview</h2>
-    <p><strong>The Polyfen Group</strong> is a small corporation parent of multiple brands related to software development and the creative industry.</p>
+    <p><a href="https://thepolyfengroup.com" target="_blank">The Polyfen Group</a> is a small corporation parent of multiple brands related to software development and the creative industry.</p>
     <p>We explore new ways of using technology to succeed in times of constant change.</p>
     <p>The Polyfen Group (internally referred to as “TPG” or “Poly”) emerged from a network of creatives, engineers, marketing people, and others from the start-up scene. It was founded in 2018 by <a href="https://boris.hr"target="_blank">Boris Hrnčić</a>, Argentinian-Croatian art director and product designer.</p>
     <p>Our location-independent business model takes advantage of the latest advancements in technology to work seamlessly with a fully-remote team years before the Covid pandemic made it the new normal.</p>
