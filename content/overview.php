21edd66da3c5921109a951a237344c28f3fd3c13
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-09-09 19:44:23 +0200

    Update overview.php

--- content/overview.php
+++ content/overview.php
@@ -1,6 +1,7 @@
 <section id="overview">
 <h2 class="heading-1">1. Overview</h2>
-  <p>The Polyfen Group LLC owns and operates a portfolio of brands with the goal to explore new ways of using technology in creative projects.</p>
+  <p>The Polyfen Group LLC is a small corporation parent of multiple brands related to software development and the creative industry.</p>
+  <p>We explore new ways of using tech in creative projects.</p>
   <p>The company was founded in 2018 by <a href="https://boris.hr" target="_blank">Boris Hrn&ccaron;i&cacute;</a> &mdash;Argentinian-Croatian, art director and product designer&mdash; with the support of a few colleagues sharing the same passion for building something we can be proud of.</p>
   <p>The stock is 100% independently owned by the team with no dependency on investors or any other kind of external funding.</p>
   <p>Our location-independent business model takes advantage of the latest advancements in technology to work seamlessly with a fully-remote team years before the Covid pandemic made it the new normal.</p>
